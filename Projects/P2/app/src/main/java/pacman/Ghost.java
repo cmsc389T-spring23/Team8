e6431102a7c8455ee2c5344ead688502afdb64c3
Nandhini Krishnan <nandhini.krishnan.1@gmail.com>
2023-02-22 15:44:35 -0500

    TA Ghost Function

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -26,6 +26,6 @@ public class Ghost {
   }
 
   public boolean attack() {
-    return false;
+    return is_pacman_in_range() && myMap.attack(myName);
   }
 }
--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
-    return;
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(9, 11), "blinky", Color.red);
+    PacMan pacman = frame.addPacMan(new Location(9,12));
+    assertTrue(ghost.attack());
   }
 }
