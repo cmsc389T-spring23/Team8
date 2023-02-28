84f193b66b5c06f1c5af68b4dd075e970c97261e
adityak-git <adityak@email.com>
2023-02-27 22:24:36 -0500

    Changes to be committed:  FINISHED	modified:   ../../../main/java/pacman/PacMan.java FINISHED	modified:   TestPacManMove.java

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
+    NoFrame frame = new NoFrame();
+    PacMan pacman = frame.addPacMan(new Location(9, 11));
+    assertTrue(pacman.move()==true);
     return;
   }
 }
