f4dd793e651274ed29a99ad517fa5aa3f53f3820
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-03-01 20:35:18 -0500

    add code  + test

--- Projects/P2/app/src/main/java/pacman/Ghost.java
+++ Projects/P2/app/src/main/java/pacman/Ghost.java
@@ -22,7 +22,21 @@ public class Ghost {
   }
 
   public boolean is_pacman_in_range() {
-    // TODO
+    int x = this.myLoc.x;
+    int y = this.myLoc.y;
+
+    for(int i = -1; i < 2; i++) {
+        for(int j = -1; j < 2; j++) {
+            for(Map.Type type : myMap.getLoc(new Location(x + j, y + j))) {
+                if (type == Map.Type.PACMAN) {
+                    if(i != 0 || j != 0) {
+                        return true;
+                    } 
+                }
+            }
+        }
+    }
+
     return false;
   }
 
