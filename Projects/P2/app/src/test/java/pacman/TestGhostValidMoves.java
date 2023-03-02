2372dfc05fb21a1ac260c479a250dbf1d48dbfb2
nicholas <nperry1215@gmail.com>
2023-03-01 19:05:14 -0500

    Ghost tests

--- Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostValidMoves.java
@@ -6,6 +6,9 @@ import java.awt.Color;
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addPacMan(new Location(6, 1));
+    assertTrue(ghost.get_valid_moves().size() == 2);
     return;
   }
 }
