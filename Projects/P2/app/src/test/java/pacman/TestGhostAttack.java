f2c45d2c1d3ef65d9e89a729fc656f584da64119
Rakrish Dhakal <dhakal.rakrish@gmail.com>
2023-03-01 20:31:06 -0500

    add code and unit test

--- Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostAttack.java
@@ -6,6 +6,10 @@ import java.awt.Color;
 public class TestGhostAttack extends TestCase {
 
   public void testGhostAttack() throws FileNotFoundException {
+    NoFrame frame = new NoFrame();
+    Ghost ghost = frame.addGhost(new Location(9, 11), "Brian", Color.YELLOW);
+    PacMan pacman = frame.addPacMan(new Location(9, 12));
+    assertTrue(pacman.is_ghost_in_range());
     return;
   }
 }
